cf5dca43d3ef6451ccecdde37c66a63f34822d61
RubyRose1 <Maddy1234@yandex.ru>
2017-11-23 17:35:50 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+/***************** Slide-In Nav ******************/
+
+$(window).load(function() {
+
+    $('.nav_slide_button').click(function() {
+        $('.pull').slideToggle();
+    });
+
+});
+
+
+/***************** Nav Transformicon ******************/
+
+document.querySelector("#nav-toggle").addEventListener("click", function() {
+    this.classList.toggle("active");
+});
+
